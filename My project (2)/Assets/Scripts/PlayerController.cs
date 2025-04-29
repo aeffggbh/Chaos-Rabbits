09b7c20d10a69671cc9fcca91c82573eddafc9fc
sofi <genuinedistinctclearcapital@gmail.com>
2025-04-29 20:37:12 -0300

    Grab and drop pistol

--- My project (2)/Assets/Scripts/PlayerController.cs
+++ My project (2)/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Drawing;
 using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEditor.PackageManager.Requests;
@@ -12,11 +13,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Character _character;
     [SerializeField] private InputActionReference _moveAction;
     [SerializeField] private InputActionReference _jumpAction;
+    [SerializeField] private InputActionReference _dropAction;
+    [SerializeField] private InputActionReference _grabAction;
     [SerializeField] private float _speed;
     [SerializeField] private float _force;
     [SerializeField] private float _counterMovementForce;
     [SerializeField] private float _jumpForce;
     [SerializeField] private CinemachineBrain _cineMachineBrain;
+    [SerializeField] private Weapon weapon;
+    [SerializeField] private float weaponBoxIncrease;
+    [SerializeField] private float maxWeaponDistance;
+    private Ray _rayFront;
+    private bool _holdingWeapon;
     private Camera _cineMachineCamera;
     private Vector3 _counterMovement;
     private Vector2 _moveInput;
@@ -27,6 +35,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable()
     {
+        weaponBoxIncrease = 1.2f;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _holdingWeapon = true;
+
         _moveInput = new Vector2(0, 0);
         if (!_moveAction)
             Debug.LogError(nameof(_moveAction) + " is null");
@@ -43,6 +58,16 @@ public class PlayerController : MonoBehaviour
             _jumpAction.action.started += OnJump;
 
 
+        if (!_dropAction)
+            Debug.LogError(nameof(_dropAction) + "is null");
+        else
+            _dropAction.action.started += DropWeapon;
+
+        if (!_grabAction)
+            Debug.LogError(nameof(_grabAction) + "is null");
+        else
+            _grabAction.action.started += GrabWeapon;
+
 
         if (!_cineMachineBrain)
             Debug.LogError(nameof(_cineMachineBrain) + " is null");
@@ -54,6 +79,69 @@ public class PlayerController : MonoBehaviour
             return;
 
         _cineMachineCamera = _cineMachineBrain.GetComponent<Camera>();
+
+    }
+
+    private void GrabWeapon(InputAction.CallbackContext context)
+    {
+        if (PointingToWeapon())
+        {
+            _holdingWeapon = true;
+            weapon.Hold();
+        }
+    }
+
+    private void DropWeapon(InputAction.CallbackContext context)
+    {
+        if (_holdingWeapon)
+        {
+            _holdingWeapon = false;
+            weapon.Drop();
+        }
+    }
+
+    /// <summary>
+    /// https://unacademy.com/content/nda/study-material/mathematics/analytical-geometry-three-dimensions-distance-between-two-points/#:~:text=Similarly%2C%20to%20calculate%20the%20distance,between%20two%20points%20is%20required.&text=PQ%20%3D%20d%20%3D%20%E2%88%9A%20%5B(x,%E2%80%93%20z1)2%5D.
+    /// </summary>
+    private bool PointingToWeapon()
+    {
+        //d = √ [(x2 – x1)2 + (y2 – y1)2 + (z2 – z1)2].
+        Vector3 start = transform.position;
+        Vector3 end = weapon.transform.position;
+        float diffX = end.x - start.x;
+        float diffY = end.y - start.y;
+        float diffZ = end.z - start.z;
+        float distance = (float)Math.Sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
+
+        Vector3 pointInView = _rayFront.origin + (_rayFront.direction * distance);
+
+        Debug.Log("Point: " + pointInView);
+
+        BoxCollider boxCollider = weapon.GetComponent<BoxCollider>();
+
+        if (boxCollider == null) return false;
+
+        Vector3 max = boxCollider.bounds.max;
+        Vector3 min = boxCollider.bounds.min;
+
+        max.x += weaponBoxIncrease;
+        max.y += weaponBoxIncrease;
+        max.z += weaponBoxIncrease;
+        min.x -= weaponBoxIncrease;
+        min.y -= weaponBoxIncrease;
+        min.z -= weaponBoxIncrease;
+         
+        //Debug.Log("Min: " + min);
+        //Debug.Log("Max: " + max);
+        Debug.Log("Distance: " + distance);
+
+        return (pointInView.x >= min.x && pointInView.x <= max.x &&
+                pointInView.y >= min.y && pointInView.y <= max.y &&
+                pointInView.z >= min.z && pointInView.z <= max.z)
+                &&
+                !_holdingWeapon
+                &&
+                distance <= maxWeaponDistance;
 
     }
 
@@ -85,6 +173,15 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (_rayFront.direction != _cineMachineCamera.transform.forward)
+            _rayFront.direction = _cineMachineCamera.transform.forward;
+
+        if (_rayFront.origin != _cineMachineCamera.transform.position)
+            _rayFront.origin = _cineMachineCamera.transform.position;
+    }
+
     private void FixedUpdate()
     {
         if (_cineMachineBrain != null)
@@ -111,6 +208,13 @@ public class PlayerController : MonoBehaviour
             _character.RequestMovement(_3DMovement);
         else
             Debug.LogError(nameof(_character) + " is null");
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = UnityEngine.Color.yellow;
+        Gizmos.DrawRay(_rayFront);
     }
 
     private void OnCancelMove(InputAction.CallbackContext context)
--- My project (2)/Assets/Scripts/Weapon.cs
+++ My project (2)/Assets/Scripts/Weapon.cs
@@ -1,23 +1,47 @@
 using System;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[Serializable]
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private Bullet prefabBullet;
-    [SerializeField] private Transform tip;
-    [SerializeField] private InputActionReference shootAction;
+    [SerializeField] private Bullet _prefabBullet;
+    [SerializeField] private Transform _tip;
+    [SerializeField] private InputActionReference _shootAction;
+    [SerializeField] private Transform _world;
+    [SerializeField] private Transform _FPCamera;
+    private Vector3 defaultPos;
+    private Vector3 rotation;
 
     private void OnEnable()
     {
-        if (shootAction)
-        {
-            shootAction.action.started += OnShoot;
-        }
+        ResetPos();
+
+
+        transform.SetParent(_FPCamera);
+
+        if (_shootAction)
+            _shootAction.action.started += OnShoot;
         else
-        {
-            Debug.LogError(nameof(shootAction) + " is null");
-        }
+            Debug.LogError(nameof(_shootAction) + " is null");
+    }
+
+    void ResetPos()
+    {
+        defaultPos = _FPCamera.transform.position;
+        //defaultPos.x += 1;
+        //defaultPos.y -= 1;
+        //defaultPos.z += 2;
+
+        defaultPos += _FPCamera.right; 
+        defaultPos += -_FPCamera.up * 1.2f; 
+        defaultPos += _FPCamera.forward * 2; 
+
+
+
+        transform.position = defaultPos;
     }
 
     private void OnShoot(InputAction.CallbackContext context)
@@ -27,10 +51,28 @@ public class Weapon : MonoBehaviour
 
     public void FireInstance()
     {
-        var newBullet = Instantiate(prefabBullet,
-                 tip.position,
-                 tip.rotation);
+        var newBullet = Instantiate(_prefabBullet,
+                 _tip.position,
+                 _tip.rotation);
 
         newBullet.Fire();
+    }
+
+    public void Hold()
+    {
+        ResetPos();
+        transform.eulerAngles = _FPCamera.eulerAngles;
+
+        transform.SetParent(_FPCamera);
+        if (GetComponent<Rigidbody>())
+            Destroy(GetComponent<Rigidbody>());
+    }
+
+    public void Drop()
+    {
+        Debug.Log("drop");
+        transform.SetParent(_world);
+        if (!GetComponent<Rigidbody>())
+            gameObject.AddComponent<Rigidbody>();
     }
 }
